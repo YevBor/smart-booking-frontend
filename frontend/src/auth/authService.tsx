b62f5c76a587538f9b6727d84c65bbf5a790cca5
Yevgeniy <yev.bor@yandex.ru>
2023-06-08 15:07:16 +0300

    few changes in daily

--- frontend/src/auth/authService.tsx
+++ frontend/src/auth/authService.tsx
@@ -22,7 +22,7 @@ export const signInUser = async (userDetails: any) => {
 export const weekly = async () =>{
   const token = localStorage.getItem('token')
   try{
-    const response = await fetch("http://localhost:4000/slots",{
+    const response = await fetch("http://localhost:4000/booking/business/5ecf94b8-9dd3-4a2a-9140-5d70cdfbc341?page=1",{
     headers: {
       'Content-Type': 'application/json',
       "Authorization": `Bearer ${token}`}
--- frontend/src/components/UI/clientCalendar/Daily.tsx
+++ frontend/src/components/UI/clientCalendar/Daily.tsx
@@ -1,136 +1,80 @@
-import dayjs,{Dayjs} from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
 import '../../../styles/daily.css';
 import { useSelector } from 'react-redux';
 import { useCallback, useEffect, useState } from 'react';
 import { Button } from '@mui/material';
 import { weekly } from '../../../auth/authService';
-import { connectStorageEmulator } from 'firebase/storage';
 
-interface Slot {
-  id: number;
-  startTime: string;
-  endTime: string;
-  status: number;
-  bookingBy: null | string; // adjust the type of `bookingBy` as needed
-}
+
 
 export const Daily = () => {
-  const daily = useSelector((state: any) => state.dailyHours.ar);
-  const startTime = daily.map((item: any) => item.startTime);
   const [active, setActive] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState([{}]);
+  const [data, setData] = useState([{ day: '', dayNumber: '', slots: [] }]);
 
   const handleClick = (index: any) => {
     setActive(index);
   };
 
-  
   const slots = useCallback(async () => {
     setIsLoading(true);
     const arr = await weekly();
-    setIsLoading(false);
-
-  //   // Get the start of the current day
-  //   const today = dayjs().startOf('day');
-
-  //  // Calculate the start and end of the week based on today
-  //   const currentWeekStart = today;
-  //   const currentWeekEnd = today.add(7, 'day');
-  //   console.log(currentWeekEnd)
-
-  //   // Filter slots for the current week
-  //   const currentWeekSlots = arr.filter((slot:any) =>
-  //     dayjs(slot.startTime).isBetween(currentWeekStart, currentWeekEnd, null, '[]')
-  //   );
-
-  //   setData(currentWeekSlots);
-  
-  //   for(let i = 0; i <7 ; i++){
-  //     const newData= arr.filter((item:any) =>{ 
-  //       return item.startTime === weeksSlots[i][2]? item: null;
-  //     })
-
-  //   }
-
-    const filterSlotsForCurrentWeek =((arr:any)=>{
+    console.log(arr)
+    
+    const filterSlotsForCurrentWeek = (arr: any) => {
       const today = dayjs().startOf('day');
-      const currentWeek ={
-        start:today,
-        end: today.add(7,'day')
-      }
-      const currentWeekSlots= []
-      for(let i = 0; i<7; i++){
-        const currentDaySlots= arr.filter((slot:any)=>
-          dayjs(slot.startTime).isSame(currentWeek.start.add(i, 'day'),'day')
+      const currentWeek = {
+        start: today,
+        end: today.add(7, 'day'),
+      };
+      const currentWeekSlots = [];
+      for (let i = 0; i < 7; i++) {
+        const currentDaySlots = arr.filter((slot: any) =>
+          dayjs(slot.startTime).isSame(currentWeek.start.add(i, 'day'), 'day')
         );
         currentWeekSlots.push({
-          day: currentWeek.start.add(i,'day').format('YYYY-MM-DD'),
-          slots:currentDaySlots
-        })
+          day: currentWeek.start.add(i, 'day').format('ddd'),
+          dayNumber: currentWeek.start.add(i, 'day').format('D'),
+          slots: currentDaySlots,
+        });
       }
-      return currentWeekSlots
-    })
-    const currentWeekSlots = filterSlotsForCurrentWeek(arr)
-    console.log(currentWeekSlots)
-    setData(currentWeekSlots)
+      return currentWeekSlots;
+    };
+    const currentWeekSlots = filterSlotsForCurrentWeek(arr);
+    setData(currentWeekSlots);
+    setIsLoading(false);
   }, []);
-
-
-
+  
   useEffect(() => {
     slots();
   }, [slots]);
 
-  const today = dayjs().format('YYYY-MM-DD');
-  const weeksSlots: any = [];
-  for (let i = 0; i < 7; i++) {
-    const day = dayjs().add(i, 'day').format('ddd,D,YYYY-MM-DD').split(',');
-    weeksSlots.push(day);
-  }
-  const todaysSlots = data.filter((slot: Slot) => {
-    const slotDay = dayjs(slot.startTime).format('YYYY-MM-DD');
-    return slotDay === today;
-  });
-
-
-
   return (
     <div style={{ width: '40%' }}>
       <div className="weekly">
-        {weeksSlots.map((item: any, index: any) => (
-          <div className="weekly__item_day" key={index}>
-            <span className="item_title">{item[0]}</span>
-            <div className="item_title-number">{item[1]}</div>
-          </div>
-        ))}
-        {/* {data.map((item:any,index)=>(
-                <div className="weekly__item_day" key={index}>
-                    <span className="item_title">{item[0]}</span>
-                    <div className="item_title-number">{item[1]}</div>
-                </div>
-        ))} */}
-      </div>
-
-      <div className="weekly">
         {data.map((innerItem: any, outerIndex: any) => (
-          <ul className="item_time" key={outerIndex}>
-            {console.log(innerItem)}
-            {data.map((item: any, innerIndex: any) => (
-              <li
-                key={outerIndex * startTime.length + innerIndex}
-                onClick={() =>
-                  handleClick(outerIndex * startTime.length + innerIndex)
-                }
-                className={`item_time-el ${
-                  active === outerIndex * startTime.length + innerIndex &&
-                  'item_time-el__active'
-                }`}
-              >
-                {item.day}
-              </li>
-            ))}
-          </ul>
+          <div>
+            <div className="weekly">
+              <div className="weekly__item_day" key={outerIndex}>
+                <span className="item_title">{innerItem.day}</span>
+                <div className="item_title-number">{innerItem.dayNumber}</div>
+              </div>
+            </div>
+
+            <ul className="item_time" key={outerIndex}>
+              {innerItem.slots.map((item: any, innerIndex: any) => (
+                <li
+                  key={item.id}
+                  onClick={() => handleClick(item.id)}
+                  className={`item_time-el ${
+                    active === item.id && 'item_time-el__active'
+                  }`}
+                >
+                  {dayjs(item.startTime).format('HH:mm')}
+                </li>
+              ))}
+            </ul>
+          </div>
         ))}
       </div>
       <Button onClick={() => weekly()}>Start</Button>
