199840185dce7ab0ee0c9a55c66409ee75051f4f
Yevgeniy <yev.bor@yandex.ru>
2023-06-10 14:58:52 +0300

    divided component

--- frontend/src/components/UI/clientCalendar/Daily.tsx
+++ frontend/src/components/UI/clientCalendar/Daily.tsx
@@ -1,59 +1,18 @@
-import dayjs, { Dayjs } from 'dayjs';
 import '../../../styles/daily.css';
-import { useSelector } from 'react-redux';
-import { useCallback, useEffect, useState } from 'react';
 import { Button } from '@mui/material';
-import { weekly } from '../../../auth/authService';
-
-
+import dayjs from 'dayjs';
+import useSlots from '../../../hooks/useSlots';
 
 export const Daily = () => {
-  const [active, setActive] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState([{ day: '', dayNumber: '', slots: [] }]);
+  const { data, handleClick, slots, active, isLoading } = useSlots();
 
-  const handleClick = (index: any) => {
-    setActive(index);
-  };
-
-  const slots = useCallback(async () => {
-    setIsLoading(true);
-    const arr = await weekly();
-    console.log(arr)
-    
-    const filterSlotsForCurrentWeek = (arr: any) => {
-      const today = dayjs().startOf('day');
-      const currentWeek = {
-        start: today,
-        end: today.add(7, 'day'),
-      };
-      const currentWeekSlots = [];
-      for (let i = 0; i < 7; i++) {
-        const currentDaySlots = arr.filter((slot: any) =>
-          dayjs(slot.startTime).isSame(currentWeek.start.add(i, 'day'), 'day')
-        );
-        currentWeekSlots.push({
-          day: currentWeek.start.add(i, 'day').format('ddd'),
-          dayNumber: currentWeek.start.add(i, 'day').format('D'),
-          slots: currentDaySlots,
-        });
-      }
-      return currentWeekSlots;
-    };
-    const currentWeekSlots = filterSlotsForCurrentWeek(arr);
-    setData(currentWeekSlots);
-    setIsLoading(false);
-  }, []);
-  
-  useEffect(() => {
-    slots();
-  }, [slots]);
-
-  return (
+  return isLoading ? (
+    <p>Loading...</p>
+  ) : (
     <div style={{ width: '40%' }}>
       <div className="weekly">
         {data.map((innerItem: any, outerIndex: any) => (
-          <div>
+          <div key={outerIndex}>
             <div className="weekly">
               <div className="weekly__item_day" key={outerIndex}>
                 <span className="item_title">{innerItem.day}</span>
@@ -61,7 +20,7 @@ export const Daily = () => {
               </div>
             </div>
 
-            <ul className="item_time" key={outerIndex}>
+            <ul className="item_time" >
               {innerItem.slots.map((item: any, innerIndex: any) => (
                 <li
                   key={item.id}
