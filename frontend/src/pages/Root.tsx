31acae4a6d20efc019c950a2a6074ff6e4a49ca1
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2023-06-11 09:42:44 +0300

    add cards to root component

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,7 +1,9 @@
-import {Box} from '@mui/material'
-import {useEffect, useState} from "react";
-import {Link} from 'react-router-dom';
+import {Box, Container} from '@mui/material'
+import React, {useEffect, useState} from "react";
 import {requestBusiness} from "../services/business/business"
+import styled from "styled-components";
+import ListComponent from "../components/UI/catalog/list-component.tsx";
+import {BusinessInfo} from "../utils/interfaces";
 const Root = () => {
   const [business,setBusiness] = useState<any>(null);
   useEffect(() => {
@@ -13,29 +15,63 @@ const Root = () => {
   }, []);
   return business && (
     <Box sx={{display:"flex",
-    justifyContent:'center',
-    alignItems:'center',
-    flexDirection:'column',
-    width:'100vw',
+      justifyContent:'center',
+      alignItems:'center',
+      flexDirection:'column',
+      width:'100vw',
     }}  >
-        <h1>Main Page</h1>
-      {business.map((biz: any)=>(
-          <div key={biz.id}>
-            <h1>here must be component CARD or something like this</h1>
-            <h2>we must render here list of featured companies/services</h2>
-            <p>This page be our top 10 of services ( For every individual who makes a payment towards this promotion )</p>
-            <p>with service name: <strong>{biz.name}</strong></p>
-            <p>with service short description: <strong>{biz.description}</strong></p>
-            <p>with service address: <strong>{biz.address}</strong></p>
-            <p>with service phone number: <strong>{biz.phoneNumber}</strong></p>
-            <div>
-             Link:  <Link to={`/biz/${biz.slug}`}>{biz.name}</Link>
-            </div>
-          </div>
-      ))}
 
+      <MainCover>
+        <Container maxWidth="lg">
+          <HeroWrapper>
+            <div>
+              <h2>Israel's Top Professionals Services,</h2>
+              <h2>Gathered Under One Roof</h2>
+              <p>This page be our top 10 of services ( For every individual who makes a payment towards this promotion )</p>
+            </div>
+            <div>
+              <Box
+                  component="img"
+                  sx={{
+                    maxHeight: { xs: '120px', md: '180px' },
+                    width: 'auto',
+                    objectFit: 'cover',
+                    borderRadius: '24px',
+                    transform: 'rotate(8deg)'
+                  }}
+                  alt='barbershop'
+                  src={`https://freedesignfile.com/upload/2019/08/Barbershop-vintage-logo-template-vector.jpg`}
+              />
+            </div>
+          </HeroWrapper>
+        </Container>
+      </MainCover>
+      <Container maxWidth="lg">
+        <ul style={{listStyle: 'none'}}>
+          {business.map((biz: BusinessInfo)=>(
+              <ListComponent biz={biz} key={biz.id} />
+          ))}
+        </ul>
+      </Container>
     </Box>
   )
 }
 
 export default Root
+const MainCover = styled.div`
+  height: 300px;
+  width: 100vw;
+  background-color: #1976d2;
+  color: white;
+  border-radius: 0 0 90% 10%/0 0 20px 0;
+  
+  @media (min-width: 700px) {
+    border-radius: 0 0 53% 47%/0 0 14% 7%;
+  }
+`
+const HeroWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 8px 16px 16px 24px;
+`
