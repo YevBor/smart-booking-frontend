eb9f2f7d78df2b3a76ea97b94710044b8d2ad5e2
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2023-06-11 09:51:22 +0300

    hero component for root page

--- frontend/src/components/hero/Hero.tsx
+++ frontend/src/components/hero/Hero.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Box, Container} from "@mui/material";
-import styled from "styled-components";
+import {HeroWrapper, MainCover} from './styles'
 
 const Hero = () => {
 	return (
@@ -33,20 +33,4 @@ const Hero = () => {
 };
 
 export default Hero;
-const MainCover = styled.div`
-  height: 300px;
-  width: 100vw;
-  background-color: #1976d2;
-  color: white;
-  border-radius: 0 0 90% 10%/0 0 20px 0;
-  
-  @media (min-width: 700px) {
-    border-radius: 0 0 53% 47%/0 0 14% 7%;
-  }
-`
-const HeroWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 8px 16px 16px 24px;
-`
+
---
+++ frontend/src/components/hero/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const MainCover = styled.div`
+  height: 300px;
+  width: 100vw;
+  background-color: #1976d2;
+  color: white;
+  border-radius: 0 0 90% 10%/0 0 20px 0;
+  
+  @media (min-width: 700px) {
+    border-radius: 0 0 53% 47%/0 0 14% 7%;
+  }
+`
+export const HeroWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 8px 16px 16px 24px;
+`
