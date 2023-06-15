e6ba635b8d8dacd18ac4c2ec01b12edee28ba732
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2023-06-15 08:08:00 +0300

    config esLint

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -4,10 +4,16 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react-refresh',
+    '@typescript-eslint/eslint-plugin'],
   rules: {
     'react-refresh/only-export-components': 'warn',
     '@typescript-eslint/no-explicit-any': 'off',
